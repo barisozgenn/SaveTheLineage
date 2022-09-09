b738ce2843b5d6b4de9d26ef6c11d22bbec039eb
Barış Özgen <barisozgen@hotmail.com>
2022-09-09 13:06:50 +0300

    Mixamo Added
    Animation names changed. Root transform rotation changed with original. Loop is enabled

---
+++ SaveTheLineage/Assets/_GameFolder/Scripts/Concretes/Controllers/MouseWorldController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SaveTheLineage.Controllers
+{
+    public class MouseWorldController : MonoBehaviour
+    {
+        [SerializeField] private LayerMask mousePlaneLayerMask;
+
+        private static MouseWorldController instance;
+
+        Ray ray;
+        float mouseRayCastMaxValue = float.MaxValue;
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        public static Vector3 GetPosition()
+        {
+            instance.ray = Camera.main.ScreenPointToRay(pos: Input.mousePosition);
+            Physics.Raycast(ray: instance.ray, hitInfo: out RaycastHit raycastHit, maxDistance: instance.mouseRayCastMaxValue, layerMask: instance.mousePlaneLayerMask);
+            return raycastHit.point;
+        }
+    }
+}
+
---
+++ SaveTheLineage/Assets/_GameFolder/Scripts/Concretes/Controllers/UnitController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SaveTheLineage.Controllers
+{
+    public class UnitController : MonoBehaviour
+    {
+        private Vector3 _targetPosition;
+
+        private float stoppingDistance = .1f;
+        float moveSpeed = 4f;
+
+        private void Update()
+        {
+
+            if (Vector3.Distance(transform.position, _targetPosition) > stoppingDistance)
+            {
+                Vector3 moveDirection = (_targetPosition - transform.position).normalized;
+
+                transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+            }
+
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Move(targetPosition: MouseWorldController.GetPosition());
+            }
+        }
+        private void Move(Vector3 targetPosition)
+        {
+            _targetPosition = targetPosition;
+        }
+    }
+
+}
